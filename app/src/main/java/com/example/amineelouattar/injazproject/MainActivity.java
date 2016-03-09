11dfa8020a4878fcbfcdd278c9425c9715df1df2
Amine Elouattar <elouattar@gmail.com>
2016-03-09 18:56:12 +0000

    Map Set up

--- app/src/main/java/com/example/amineelouattar/injazproject/MainActivity.java
+++ app/src/main/java/com/example/amineelouattar/injazproject/MainActivity.java
@@ -3,11 +3,54 @@ package com.example.amineelouattar.injazproject;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap googleMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map)).getMapAsync(this);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+        this.googleMap = googleMap;
+        mapSetUp();
+
+    }
+
+    public void mapSetUp(){
+
+        this.googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        this.googleMap.setMyLocationEnabled(true);
+        this.googleMap.getUiSettings().setZoomControlsEnabled(true);
+
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(new LatLng(33.986792, -6.850467))
+                .zoom(16)
+                .tilt(1)
+                .build();
+
+        this.googleMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+
     }
 }
---
+++ app/src/main/java/com/example/amineelouattar/injazproject/ressources/GlobalVars.java
@@ -0,0 +1,9 @@
+package com.example.amineelouattar.injazproject.ressources;
+
+/**
+ * Created by AmineElouattar on 3/9/16.
+ */
+public class GlobalVars {
+
+    public static final String URL = "";
+}
